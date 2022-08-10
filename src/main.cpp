6c1a3fd21310b2253d1e820f8a2fd24f6a3282e2
Xingrui <xingruichen2@gmail.com>
2022-08-10 16:23:20 -0700

    Update main.cpp
    title screen and game over screen

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,9 @@ using namespace vex;
 #define ScreenW 480
 
 const int scrollSpeed=2;
-const int pickupRange=13;
+const int pickupRange=15;
+const int hitboxSize=5;
+bool gameRunning=0;
 
 class Bird{
   private:
@@ -79,11 +81,34 @@ class Coin{
   int gety(){return y;}
 };
 
-std::vector<Coin*> coins;
+class Pillar{
+  private:
+  int x=ScreenW,y;
+  public:
+  const int width=50;
+  const int gap=70;
+  Pillar(){
+    y=rand()*1.0/RAND_MAX*100+80;
+  }
+  void draw(){
+    Brain.Screen.drawRectangle(x, y, width, ScreenH-y);
+    Brain.Screen.drawRectangle(x, 0, width, y-gap);
+  }
+  void Update(){
+    x-=scrollSpeed;
+    draw();
+  }
 
-bool isColliding(Coin c){ // checks if the bird is colliding with Coin c
-  // Brain.Screen.printAt(20, 40, "bird x=%d y=%d", bird.getx(), bird.gety());
-  if(abs(bird.getx()-c.getx())<pickupRange && abs(bird.gety()-c.gety())<pickupRange) return 1;
+  int getx(){return x;}
+  int gety(){return y;}
+};
+
+std::vector<Pillar*> pillars;
+
+bool isColliding(Pillar p){ // checks if the bird is colliding with Pillar p
+  if(bird.getx()>p.getx()-hitboxSize && bird.getx()<p.getx()+p.width+hitboxSize){
+    if(bird.gety()>p.gety()-hitboxSize || bird.gety()<p.gety()-p.gap+hitboxSize) return 1;
+  }
   return 0;
 }
 
@@ -91,37 +116,72 @@ int cnt=0;
 
 void Update(){
   Brain.Screen.clearScreen();
-  bird.Update();
   if(cnt==0){
-    coins.push_back(new Coin);
+    pillars.push_back(new Pillar);
   }
+  if(cnt%30==0) bird.score++;
   cnt++;
-  cnt%=30;
+  cnt%=90;
   // Brain.Screen.printAt(1, 60, "number of coins in the vector: %d", coins.size());
-  for(int i=0; i<coins.size(); i++){
-    if(coins[i]->getx()<0){
-      delete coins[i];
-      coins.erase(coins.begin()+i);
+  for(int i=0; i<pillars.size(); i++){
+    if(pillars[i]->getx() < -60){
+      delete pillars[i];
+      pillars.erase(pillars.begin()+i);
     }
-    if(isColliding(*coins[i])){
-      bird.score++;
-      delete coins[i];
-      coins.erase(coins.begin()+i);
+    if(isColliding(*pillars[i])){
+      for(int i=0; i<pillars.size(); i++){
+        delete pillars[i];
+        pillars.erase(pillars.begin()+i);
+      }
+      gameRunning=0;
     }
-    coins[i]->Update();
+    pillars[i]->Update();
+  }
+  bird.Update();
+}
+
+void titleScreen(){
+  Brain.Screen.clearScreen();
+  int midy=ScreenH/2, midx=ScreenW/2;
+  Brain.Screen.printAt(midx-45, midy+10, "Press A to start");
+  wait(1, sec);
+  while(1){
+    if(Controller1.ButtonA.pressing()) return;
+    wait(50, msec);
+  }
+}
+
+void gameOver(){
+  bird.score=0;
+  Brain.Screen.clearScreen();
+  Brain.Screen.setFont(mono30);
+  Brain.Screen.printAt(1, ScreenH/2, "GAME OVER. Press A to continue...");
+  Brain.Screen.setFont(mono20);
+  wait(1, sec);
+  while(1){
+    if(Controller1.ButtonA.pressing()) return;
+    wait(50, msec);
+  }
+}
+
+void gameLoop(){
+  while(gameRunning){
+    Update();
+    if(Controller1.ButtonA.pressing()){
+      bird.jump();
+    }
+    wait(20, msec);
   }
 }
 
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
-  
   while(1){
-    Update();
-    if(Controller1.ButtonA.pressing()){
-      bird.jump();
-    }
-    wait(20, msec);
+    titleScreen();
+    gameRunning=1;
+    gameLoop();
+    gameOver();
   }
 
 }
